9e8da82468221a9c5cd5203f5115b4138a4f0c08
chetanwani <53759208+chetanwani@users.noreply.github.com>
2022-12-19 14:47:12 +0530

    All file added

---
+++ C project/range.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	int i,range;
+	{
+		printf("\n enter a range");
+		scanf("\n%d",&range);
+	}
+	
+	for(i=1;i<=range;i++)
+	printf("%d\n",i);
+}
---
+++ C project/sum100.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+main()
+{
+	int i,sum;
+	for(i=1;i<=100;i++)
+	sum=sum+i;
+	printf("\n %d",sum);
+}
